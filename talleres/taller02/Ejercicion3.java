98ed1d81e92b0a4fb1c90e163668e75068866a26
jjaraujoa <68928341+jjaraujoa@users.noreply.github.com>
2020-08-07 11:33:18 -0500

    Add files via upload

---
+++ talleres/taller02/Ejercicion3.java
@@ -0,0 +1,29 @@
+
+
+public class Ejercicion3
+{ 
+    //Profe no supimos hacerlo ni viendo el spoiler :( porfa no nos mate
+    
+  
+    private static boolean SumaGrupoAvanzado(int start, int[] nums, int target) {
+	if(start >= nums.length){
+	    return target == 0;
+	}
+        else
+            return SumaGrupoAvanzado(start + 1, nums, target) || SumaGrupoAvanzado( start+1, nums, target - nums[start]);
+    }
+
+     public static void combinations(String s) {
+	combinationsAux("", s);
+    }
+  
+  private static void combinationsAux(String prefix, String s) {
+	if(s.length()== 0) {
+	    System.out.println(s);
+	}else{
+	    combinationsAux( prefix + s.charAt(0), s.substring(1));
+	    combinationsAux( prefix, s.substring(1));
+	}
+    }
+}
+
