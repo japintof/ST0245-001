fd034ce278f65e0da73cf8b346a9803b1c50b8ed
japintof <68928353+japintof@users.noreply.github.com>
2020-08-23 17:24:51 -0500

    Add files via upload

---
+++ laboratorios/lab01/ejercicioEnLinea/ArrayMax.java
@@ -0,0 +1,17 @@
+
+
+public class ArrayMax
+{
+    public static int arrayMax(int[] array, int n) {
+		
+        int i, max, temp;
+	max = array[n];
+	if(n==0){
+	    temp = arrayMax(array, 0 );
+	    if(temp < max)
+		max = temp;
+	}
+	return max;
+
+    }
+}
